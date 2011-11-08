ded561cce4f16759bad69d1ee4921691e64b5ed2
Antonio J. García Lagar <aj@garcialagar.es>
2011-11-08 08:31:24 +0100

    Minor source code improvements

--- source.js
+++ source.js
@@ -2,14 +2,15 @@
 // @name        Google Reader Compacter
 // @namespace   http://aj.garcialagar.es/gmscripts
 // @description To compact the new Google Reader Interface
-// @version	1.0.1
+// @version     1.0.2
 // @include     http://www.google.*/reader/*
 // @include     https://www.google.*/reader/*
 // ==/UserScript==
 
-// @author	Antonio J. García Lagar <aj@garcialagar.es>
-// @copyright	Copyright (c) 2011, Antonio J. García Lagar <aj@garcialagar.es>
-// @license	http://www.opensource.org/licenses/BSD-3-Clause
+// @author      Antonio J. García Lagar <aj@garcialagar.es>
+// @copyright   Copyright (c) 2011, Antonio J. García Lagar <aj@garcialagar.es>
+// @license     http://www.opensource.org/licenses/BSD-3-Clause
+// @homepage    https://github.com/ajgarlag/google-reader-compacter
 
 var styleText='';
 
@@ -29,14 +30,17 @@ styleText += ' #entries.list .collapsed .entry-icons {top: 0}';
 styleText += ' #entries.list .collapsed .entry-main .entry-source-title {top: 0}';
 styleText += ' #entries.list .collapsed .entry-secondary {top: 0}';
 
-/* Write down the style
--------------------------------------------------------------------------------------------------------- */
-
-var head = document.getElementsByTagName('head')[0],
-    style = document.createElement('style'),
-    rules = document.createTextNode(styleText);
+// Write down the style
+var head = document.getElementsByTagName('head')[0];
+var style = document.createElement('style');
+var rules = document.createTextNode(styleText);
 
 style.type = 'text/css';
-if(style.styleSheet)  style.styleSheet.cssText = rules.nodeValue;
-else style.appendChild(rules);
+
+if (style.styleSheet) {
+    style.styleSheet.cssText = rules.nodeValue;
+} else {
+    style.appendChild(rules);
+}
+
 head.appendChild(style);
